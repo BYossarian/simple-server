0e48bd4a0d533ce3759fd101a0edcd06d090b977
Ben Jackson <benjackson@Joshs-iMac.local>
2015-02-20 11:26:55 +0000

    major refactor, plus allow single page apps

--- server.js
+++ server.js
@@ -12,55 +12,86 @@ var http = require('http'),
 
 // error messages
 var errMsgs = {
-    badReq: "<h1>Bad request.</h1>",
-    notFound: "<h1>File not found.</h1>",
-    serverErr: "<h1>Server error.</h1>",
-    badMethod: "<h1>HTTP method not allowed. Use GET or HEAD.</h1>"
+    badReq: '<h1>Bad request.</h1>',
+    notFound: '<h1>File not found.</h1>',
+    serverErr: '<h1>Server error.</h1>',
+    badMethod: '<h1>HTTP method not allowed. Use GET or HEAD.</h1>'
 };
 
 // parse arguments and set up options
 commander
     .option('-p, --port <port>', 'The port to listen on (default: 8080)')
-    .option('-r --root <root>', 'The public file directory (default: current directory)')
+    .option('-r, --root <root>', 'The public file directory (default: current directory)')
+    .option('-s, --single [page]', 'Set single page web app entry page (default: index.html)')
     .parse(process.argv);
 
 // defaults
 var port = commander.port || 8080,
-    localRoot = process.cwd();
+    localRoot = process.cwd(),
+    entryPage = '';
+
+if (typeof commander.single === 'string') {
+    entryPage = commander.single;
+} else if (commander.single) {
+    entryPage = 'index.html';
+}
 
 if (commander.root) {
     localRoot = path.resolve(process.cwd(), commander.root);
 }
 
-http.createServer(function(req, res) {
+function sanitiseURL(requestUrl) {
 
-    var method = req.method,
-        reqPath = "",
-        headers = {};
+    // protect against malformed URLs (in which case decodeURI throws an exception)
+    try {
+        
+        requestUrl = decodeURIComponent(url.parse(requestUrl).pathname);
 
-    function log(code) {
-
-        var style = null;
-
-        switch(Math.floor(code/100)) {
-            case 2:
-                style = [32, 39];
-                break;
-            case 4:
-            case 5:
-                style = [31, 39];
-                break;
-            default:
-                style = [39, 39];
-        }
-
-        console.log("    \u001b[" + style[0] + "m" + code + "\u001b[" + style[1] + "m " + method + " " + reqPath);
+        // prevent any attempt to move up the directory structure using something like ../ or /../
+        requestUrl = path.join('/', requestUrl);
+            
+    } catch(e) {
+        
+        return;
 
     }
 
+    return requestUrl;
+
+}
+
+function log(status, req) {
+
+    var style = null;
+
+    switch(Math.floor(status/100)) {
+        case 2:
+            style = [32, 39];
+            break;
+        case 4:
+        case 5:
+            style = [31, 39];
+            break;
+        default:
+            style = [39, 39];
+    }
+
+    console.log('    \u001b[' + style[0] + 'm' + status + '\u001b[' + style[1] + 'm ' + req.method + ' ' + req.path);
+
+}
+
+http.createServer(function(request, res) {
+
+    var req = {
+        original: request,
+        method: request.method,
+        path: '',
+        localPath: ''
+    };    
+
     function respond(code, type, size, body) {
 
-        log(code);
+        log(code, req);
 
         type = type || 'text/plain; charset=utf-8';
 
@@ -69,28 +100,25 @@ http.createServer(function(req, res) {
             'Content-Length': size
         });
 
-        if (method !== "HEAD") {
-
-            if (body instanceof fs.ReadStream) {
-
-                body.on('error', function(err) {
-
-                    // TODO
-                    console.log('file stream error');
-
-                });
-
-                body.pipe(res);
-
-            } else {
-                res.end(body);
-            }
-            
-        } else {
-
+        if (req.method === 'HEAD') {
             // send only headers for HEAD request
-            res.end();
-        }        
+            return res.end();
+        }
+
+        if (body instanceof fs.ReadStream) {
+
+            body.on('error', function(err) {
+
+                // TODO
+                console.log('file stream error');
+
+            });
+
+            body.pipe(res);
+
+        } else {
+            res.end(body);
+        }   
 
     }
 
@@ -98,68 +126,64 @@ http.createServer(function(req, res) {
 
         if (err) {
 
-            if (err.code = "ENOENT") {
+            if (err.code === 'ENOENT') {
 
-                respond(404, "", errMsgs.notFound.length, errMsgs.notFound);
+                // single page app returns entry page:
+                if (entryPage) {
+                    return processReq(entryPage);
+                }
 
-            } else {
-
-                respond(500, "", errMsgs.serverErr.length, errMsgs.serverErr);
+                return respond(404, '', errMsgs.notFound.length, errMsgs.notFound);
 
             }
+
+            return respond(500, '', errMsgs.serverErr.length, errMsgs.serverErr);
+
+        }
+
+        if (stats.isFile()) {
+            // file
+
+            respond(200, mime.contentType(path.basename(req.localPath)), stats.size, fs.createReadStream(req.localPath));
+
+        } else if (stats.isDirectory()) {
+            // directory
+
+            processReq(path.join(req.path, '/index.html'));
 
         } else {
 
-            if (stats.isFile()) {
-                // file
-
-                respond(200, mime.contentType(path.basename(reqPath)), stats.size, fs.createReadStream(reqPath));
-
-            } else if (stats.isDirectory()) {
-                // directory
-
-                reqPath = reqPath.replace(/\/*$/,"/index.html");
-                fs.stat(reqPath, processFile);
-
-            } else {
-
-                respond(400, "", errMsgs.badReq.length, errMsgs.badReq);
-
-            }
+            respond(400, '', errMsgs.badReq.length, errMsgs.badReq);
 
         }
 
     }
 
-    // protect against malformed URLs (in which case decodeURI throws an exception)
-    try {
-        
-        reqPath = decodeURIComponent(url.parse(req.url).pathname);
+    function processReq(reqUrl) {
 
-        // prevent any attempt to move up the directory structure using something like ../ or /../
-        reqPath = path.join('/', reqPath);
+        // handle only GET and HEAD requests for now:
+        if (req.method !== 'GET' && req.method !== 'HEAD') {
+            return respond(405, '', errMsgs.badMethod.length, errMsgs.badMethod);
+        }
+
+        req.path = sanitiseURL(reqUrl);
+
+        if (typeof req.path !== 'string') {
+            // malformed URL
+
+            return respond(400, '', errMsgs.badReq.length, errMsgs.badReq);
+        }
+
+        req.localPath = path.join(localRoot, req.path);
         
-        reqPath = path.join(localRoot, reqPath);
-    
-    } catch(e) {
-        
-        respond(400, "", errMsgs.badReq.length, errMsgs.badReq);
-        return;
+        fs.stat(req.localPath, processFile);
 
     }
-    
-    if (method === "GET" || method === "HEAD") {
 
-        fs.stat(reqPath, processFile);
-
-    } else {
-        
-        respond(405, "", errMsgs.badMethod.length, errMsgs.badMethod);
-
-    }
+    processReq(req.original.url);
 
 }).listen(port, function() {
 
-    console.log("\n  Server running on port " + port + " with a root of " + localRoot + "\n");
+    console.log('\n  Server running on port ' + port + ' with a root of ' + localRoot + '\n');
 
 });
